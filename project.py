8be448e08d12675091e60e78c1afcb05acd07664
reshamchaney <56357456+reshamchaney@users.noreply.github.com>
2019-10-31 21:45:53 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,315 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+''' tk_Canvas_BGImage1.py
+use a Tkinter canvas for a background image
+for result see http://prntscr.com/12p9ux
+'''
+try:
+    # Python2
+    import Tkinter as tk
+except ImportError:
+    # Python3
+    import tkinter as tk
+root = tk.Tk()
+root.title('background image')
+# pick a .gif image file you have in the working directory
+fname = "cartoon_birds_blue_flying_animation_clipart.gif"
+bg_image = tk.PhotoImage(file=fname)
+# get the width and height of the image
+w = bg_image.width()
+h = bg_image.height()
+# size the window so the image will fill it
+root.geometry("%dx%d+50+30" % (w, h))
+cv = tk.Canvas(width=w, height=h)
+cv.pack(side='top', fill='both', expand='yes')
+cv.create_image(0, 0, image=bg_image, anchor='nw')
+# add canvas text at coordinates x=15, y=20
+# anchor='nw' implies upper left corner coordinates
+cv.create_text(15, 20, text="Python Greetings", fill="red", anchor='nw')
+# now add some button widgets
+btn1 = tk.Button(cv, text="Click")
+btn1.pack(side='left', padx=10, pady=5, anchor='sw')
+btn2 = tk.Button(cv, text="Quit", command=root.destroy)
+btn2.pack(side='left', padx=10, pady=5, anchor='sw')
+root.mainloop()
+
+
+# In[5]:
+
+
+from tkinter import *
+window=Tk()
+window.title("log in/sign up")
+fname ="71JnoSPl2hL._SL1134_ (1).gif" 
+bg_image=PhotoImage(file=fname)
+w=bg_image.width()
+h = bg_image.height()
+window.geometry("%dx%d"%(w+100,h+100))
+canvas = Canvas(window,width=w+50, height=h+50)
+canvas.pack()
+canvas.create_image(0, 0, image=bg_image,anchor="nw")
+#l1=Label(canvas,text="user name :")
+#l1.grid(row=1,column=1)
+window.mainloop()
+
+
+# In[86]:
+
+
+from random import *
+from  tkinter import *
+from  tkinter import messagebox
+details=[[1,"resham","9304619889","20","jamshedpur","resham.chaney1099@gmail.com","abc"],[2,"harsha","7209534724","24","jamshedpur","harshachaney95@gmail.com","xyz"]]
+c=2
+window=Tk()
+window.title("log in/sign up")
+window.geometry("1000x1000")
+
+def signup():
+    reg=Toplevel(window)
+    reg.title("sign up")
+    reg.geometry("1000x1000")
+    frame=Frame(reg,width=600,height=600,bg="pink",padx=100,pady=50)
+    frame.pack()
+    
+    def verify():#called on clicking register button
+        f1=f2=f3=f4=f5=f35=0
+        l=[]
+        tag1=Message(frame,text="",bg="pink",width=100)
+        tag1.grid(row=4,column=2)
+        tag2=Message(frame,text="",bg="pink",width=100)
+        tag2.grid(row=6,column=2)
+        tag3=Message(frame,text="",bg="pink",width=100)
+        tag3.grid(row=8,column=2)
+        tag35=Message(frame,text="",bg="pink",width=100)
+        tag35.grid(row=10,column=2)
+        tag4=Message(frame,text="",bg="pink",width=100)
+        tag4.grid(row=12,column=2)
+        tag5=Message(frame,text="",bg="pink",width=100)
+        tag5.grid(row=15,column=2)
+        
+        #name
+        s=en1.get()
+        if (s.isalpha() or " " in s):
+            f1=1
+        else:
+             tag1.config(text="Invalid name")
+        #phone no
+        if (en2.get().isdigit() and len(en2.get())==10 ):
+            f2=1
+        else:
+            tag2.config(text="Invalid phone number")
+        #age
+        if (not (en3.get().isdigit())):
+            tag3.config(text="Invalid age")
+        elif int(en3.get())>120 :
+            tag3.config(text="Invalid age")
+        else:
+            f3=1
+        #city
+        if en4.get().isalpha():
+            f35=1
+        else:
+             tag35.config(text="Invalid city")
+        #email id
+        s=en5.get()
+        if("@" in en5.get() and "." in en5.get()):
+            if(s[0]!="@" and s[0]!="." and s[len(s)-1]!="@" and s[len(s)-1]!="." and " " not in s):
+                if((s.find("@")+1)!=s.find(".") or (s.find(".")+1)!=s.find("@")):
+                    f4=1
+                else:
+                    tag4.config(text="Invalid email id")
+            else:
+                tag4.config(text="Invalid email id") 
+        else:
+            tag4.config(text="Invalid email id")
+        #password cofirmation
+        if(en6.get()=="" or en7.get()==""):
+            tag5.config(text="Enter both the fields")
+        elif(en6.get()==en7.get()):
+            f5=1
+        else:
+            tag5.config(text="Password does not match")
+        
+        #to remove the labels if the value is corrected
+        if(f1==1):
+            print("entered")
+            tag1.config(text="                 ",bg="pink")
+        if(f2==1):
+            tag2.config(text="                             ",bg="pink")
+        if(f3==1):
+            tag3.config(text="                             ",bg="pink")
+        if(f35==1):
+            tag35.config(text="                             ",bg="pink")
+        if(f4==1):
+            tag4.config(text="                             ",bg="pink")
+        if(f5==1):
+            tag5.config(text="                             ",bg="pink")
+        
+        # if all the fiels are correct send otp verification to admin
+        if f1==1 and f2==1 and f3==1 and f35==1 and f4==1 and f5==1:
+            n=str(123456)# arbitrary value for checking the working
+            #n=sendMail(en5.get()) #admin function call
+            print("in the email confirmation")
+            email=Toplevel(reg)#email confirmation window
+            email.title("email confirmation")
+            email.geometry("800x500")
+            frame1=Frame(email,height=400,width=800,bg="grey",padx=50,pady=50)
+            frame1.pack()
+            m=Message(frame1,text="You have recieved a confirmation mail. Please enter the OTP below to confirm the email id.",width=600)
+            m.grid(row=1,columnspan=4,column=1)
+            sp1=Label(frame,bg="grey")
+            sp1.grid(row=2)
+            def success():#when signup is successful it is added to the list which could further be updated in the database
+                if(entry.get()==n):
+                    global c
+                    c=c+1
+                    l.append(c)
+                    l.append(en1.get())
+                    l.append(en2.get())
+                    l.append(en3.get())
+                    l.append(en4.get())
+                    l.append(en5.get())
+                    l.append(en6.get())
+                    print (l)
+                    details.append(l)
+                    print (details)
+                    messagebox.showinfo("Information","Congratualtions! You have successfully resgistered....!!!")
+            label=Label(frame1,text="Enter the OTP : ")
+            label.grid(row=3,column=3)
+            otp=StringVar()
+            entry=Entry(frame1,textvariable=otp)
+            entry.grid(row=3,column=4)
+            sp1=Label(frame,bg="yellow")
+            sp1.grid(row=4)
+            submit=Button(frame1,text="submit",bg="green",fg="white",command=success)
+            submit.grid(row=5,columnspan=4,column=4)
+            
+        
+        
+        
+    head=Label(frame,text="SIGN UP",bg="pink")
+    head.config(font=("Times new roman", 44))
+    head.grid(row=1,columnspan=2,column=1)
+    sp1=Label(frame,text="   ",bg="pink")
+    sp1.grid(row=2)
+    
+    lab1=Label(frame,text="Full Name : ",bg="pink",justify=RIGHT)
+    name=StringVar()
+    en1=Entry(frame,textvariable=name)
+    lab1.grid(row=3,column=1)
+    en1.grid(row=3,column=2)
+    
+    
+    lab2=Label(frame,text="Phone Number : ",bg="pink",justify=RIGHT)
+    phone=StringVar()
+    en2=Entry(frame,textvariable=phone)
+    lab2.grid(row=5,column=1)
+    en2.grid(row=5,column=2)
+    
+    
+    lab3=Label(frame,text="Age : ",bg="pink",justify=RIGHT)
+    age=StringVar()
+    en3=Entry(frame,textvariable=age)
+    lab3.grid(row=7,column=1)
+    en3.grid(row=7,column=2)
+    
+    lab4=Label(frame,text="City : ",bg="pink",justify=RIGHT)
+    city=StringVar()
+    en4=Entry(frame,textvariable=city)
+    lab4.grid(row=9,column=1)
+    en4.grid(row=9,column=2)
+    
+    lab5=Label(frame,text="Email Id : ",bg="pink",justify=RIGHT)
+    email=StringVar()
+    en5=Entry(frame,textvariable=email)
+    lab5.grid(row=11,column=1)
+    en5.grid(row=11,column=2)
+    
+    lab6=Label(frame,text="Password : ",bg="pink",justify=RIGHT)
+    password=StringVar()
+    en6=Entry(frame,textvariable=password,show="*")
+    lab6.grid(row=13,column=1)
+    en6.grid(row=13,column=2)
+    
+    lab7=Label(frame,text="Confirm Password : ",bg="pink",justify=RIGHT)
+    confirm=StringVar()
+    en7=Entry(frame,textvariable=confirm,show="*")
+    lab7.grid(row=14,column=1)
+    en7.grid(row=14,column=2)
+    
+    sp1=Label(frame,bg="pink")
+    sp1.grid(row=16)
+    
+    register=Button(frame,text="RESGISTER",bg="red",fg="white",command=verify)
+    register.grid(row=17,columnspan=2,column=1)
+    
+    #reg.mainloop()
+
+def login():
+    f=0
+    if(e1.get()==""):
+        messagebox.showinfo("Information","Please enter username....!!!")
+    else:
+        for i in details:
+            if(e1.get()==i[5]):
+                f=1
+                if(e2.get()==""):
+                    messagebox.showwarning("Warning","Please enter password....!!!")
+                elif(e2.get()==i[6]):
+                    messagebox.showinfo("Information","You have successfully logged in....!!!")
+                else:
+                    messagebox.showwarning("Warning","You have entered the wrong password....!!!")
+        if(f==0):
+            messagebox.showinfo("Information","This username does not exist!You need to Sign up....!!!")
+
+def adminlogin():#called on clickin the admin login button. To be edited by the admin page creator
+    pass;
+    
+
+frame=Frame(window,width=600,height=600,bg="yellow",padx=100,pady=50)
+frame.pack()
+
+head=Label(frame,text="LOG IN",bg="yellow")
+admin=Button(frame,bg="black",fg="white",text="ADMIN LOGIN",anchor="e",command=adminlogin())
+admin.grid(row=10,column=3)
+head.config(font=("Times new roman", 44))
+
+sp1=Label(frame,bg="yellow")
+sp1.grid(row=3)
+
+lb1=Label(frame,text="User Name : ",bg="yellow")
+username=StringVar()
+e1=Entry(frame,textvariable=username)
+head.grid(row=2,columnspan=2,column=1)
+lb1.grid(row=4,column=1)
+e1.grid(row=4,column=2)
+
+lb2=Label(frame,text="Password : ",bg="yellow")
+password=StringVar()
+e2=Entry(frame,textvariable=password,show="*")
+lb2.grid(row=5,column=1)
+e2.grid(row=5,column=2)
+
+sp1=Label(frame,bg="yellow")
+sp1.grid(row=6)
+
+log=Button(frame,bg="green",fg="white",text="LOG IN",command=login)
+log.grid(row=7,columnspan=2,column=1)
+
+sp1=Label(frame,bg="yellow")
+sp1.grid(row=8)
+sp1=Label(frame,bg="yellow")
+sp1.grid(row=9)
+
+#lb3=Label(frame,text="                New user ? ",bg="yellow",anchor="e")
+sign=Button(frame,bg="black",fg="white",text="SIGN UP",anchor="w",command=signup)
+#lb3.grid(row=9,column=3)
+sign.grid(row=10,column=4)
+
+window.mainloop()
+
